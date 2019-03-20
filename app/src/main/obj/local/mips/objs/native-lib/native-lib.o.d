6bc286b4ae9c5b882edf01e6109723d6182ba133
zhouxiaoli521 <yy315614247@163.com>
2019-03-20 21:21:56 +0800

    so

---
+++ app/src/main/obj/local/mips/objs/native-lib/native-lib.o.d
@@ -0,0 +1,80 @@
+C:/Users/yangyu/AndroidStudioProjects/MyApplication/app/src/main//obj/local/mips/objs/native-lib/native-lib.o: \
+ C:/Users/yangyu/AndroidStudioProjects/MyApplication/app/src/main//jni/native-lib.cpp \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/jni.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/string.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/android/api-level.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/malloc.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/assert.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/stdlib.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/alloca.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/strings.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/stdint.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/_types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/machine/_types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/posix_types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/stddef.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/compiler.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/posix_types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/sgidefs.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/types.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/machine/kernel.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/sysmacros.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/memory.h \
+ C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/ctype.h \
+ C:/Users/yangyu/AndroidStudioProjects/MyApplication/app/src/main//jni/native-lib.h
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/jni.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/string.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/android/api-level.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/malloc.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/assert.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/stdlib.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/alloca.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/strings.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/stdint.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/_types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/machine/_types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/posix_types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/stddef.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/compiler.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/posix_types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/sgidefs.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/asm/types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/linux/types.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/machine/kernel.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/sys/sysmacros.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/memory.h:
+
+C:/android-ndk-r8d/platforms/android-9/arch-mips/usr/include/ctype.h:
+
+C:/Users/yangyu/AndroidStudioProjects/MyApplication/app/src/main//jni/native-lib.h:
